86922db09e3586842bf2f797b44598d9179c91ae
yiwang <yiwang03@ucm.es>
2022-03-21 14:55:57 +0100

    Añadiendo fichero scrollPieces 2

---
+++ scrollPieces.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace scrollPieces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            while (true)
+            {
+                Console.CursorVisible = false;
+                Console.CursorVisible = true;
+            }
+        }
+    }
+}

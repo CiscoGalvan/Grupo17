f5fb75eaf74cfe68f8ff98f232d134f094d5f925
fragalva <fragalva@ucm.es>
2022-03-21 11:32:11 +0100

    Añadimos los colores azul y rojo

---
+++ addColor/ColorToRed/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ColorToRed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("                            ");
+        }
+    }
+}

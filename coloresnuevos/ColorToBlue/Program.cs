0cf173f9ca289a20c3f3f4fd143b4efca7cf6f92
fragalva <fragalva@ucm.es>
2022-03-21 11:59:38 +0100

    Subida de los colores

---
+++ coloresnuevos/ColorToBlue/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ColorToBlue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write("                            ");
+        }
+    }
+}
